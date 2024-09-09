352efaa686a473ac7dfa32379445dc1dfb8d0f02
Mjumbe Poe <mjumbewu@gmail.com>
2024-09-09 14:14:55 -0400

    Initial files for week 2

---
+++ week02/README.md
@@ -0,0 +1,19 @@
+## Videos
+
+* The GeoJSON Data Format -- Coming Soon!
+* Intro to Web Maps -- Coming Soon!
+* Asynchronous Behavior #1 (events) -- Coming Soon!
+
+## Readings
+
+From [JavaScript.info](https://javascript.info/):
+* Part 1, Chapter 4 -- Objects: the basics **(only section 4.1 is necessary)**
+* Part 1, Chapter 5 -- Data types **(specifically sections 5.2, 5.3, and 5.4 -- the rest are optional)**
+
+From [Introduction to Web Mapping](https://geobgu.xyz/web-mapping2/):
+* Chapter 7 -- GeoJSON, https://geobgu.xyz/web-mapping2/geojson-1.html
+
+## References
+
+* _JavaScript modules_, from MDN Web Docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+* The GeoJSON Specification, https://datatracker.ietf.org/doc/html/rfc7946
---
+++ week02/lecture_examples/geojson_data/js/main.js
@@ -0,0 +1,55 @@
+//
+// Create a new Leaflet map object to be displayed in the #map div
+//
+const element = document.querySelector("#map");
+const map = L.map(element, { maxZoom: 18 });
+
+//
+// Add a base layer to the map
+//
+const mapboxStyle = "mapbox/streets-v11";
+const mapboxKey = "pk.eyJ1IjoibWp1bWJlLXRlc3QiLCJhIjoiY2w3ZTh1NTIxMTgxNTQwcGhmODU2NW5kaSJ9.pBPd19nWO-Gt-vTf1pOHBA";
+
+const baseLayer = L.tileLayer(
+  `https://api.mapbox.com/styles/v1/${mapboxStyle}/tiles/{z}/{x}/{y}{r}?access_token=${mapboxKey}`,
+  { zoomOffset: -1, tileSize: 512 }
+);
+baseLayer.addTo(map);
+
+//
+// Load GeoJSON data from a file and add it to the map
+//
+const response = await fetch("shapes.json");
+const data = await response.json();
+
+const dataLayer = L.geoJSON(data, {
+  pointToLayer: (feature, latlng) => L.circleMarker(latlng),
+  style: (feature) =>
+    feature.geometry.type === "Point"
+      ? { stroke: false, radius: 6, fillOpacity: 1 }
+      : {},
+    interactive: true,
+});
+dataLayer.addTo(map);
+dataLayer.addEventListener("mouseover", (event) => {
+  const layer = event.layer;
+  const feature = event.layer.feature;
+  const latlng = event.latlng;
+
+  layer.tooltip = (layer.tooltip || L.tooltip())
+    .setLatLng(latlng)
+    .setContent(feature.properties.name)
+    .addTo(map);
+});
+
+dataLayer.addEventListener("mouseout", (event) => {
+  const layer = event.layer;
+  if (layer.tooltip) {
+    layer.tooltip.removeFrom(map);
+  }
+});
+
+//
+// Fit the map to the bounds of the GeoJSON data
+//
+map.fitBounds(dataLayer.getBounds());
