694db24fdfb09e147670111875015694e30f5f97
Mjumbe Poe <mjumbewu@gmail.com>
2025-10-29 14:39:24 -0400

    Add autocomplete functionality to week 9 example

--- week09/lecture_examples/address_autocomplete/js/location.js
+++ week09/lecture_examples/address_autocomplete/js/location.js
@@ -35,12 +35,47 @@ function initGeolocation(events) {
  */
 function initAutocomplete(events) {
   const apiUrlBase = 'https://geocoding.geo.census.gov/geocoder/locations/address?city=Philadelphia&state=PA&benchmark=4&format=json&street=';
-  autocompleteInput.addEventListener('input', _.debounce(async () => {
+  
+  const inputHandler = async () => {
     console.log('Input changed:', autocompleteInput.value);
+
+    // TODO: We should not send a request for empty input.
+
     const response = await fetch('https://corsproxy.io/?url=' + apiUrlBase + encodeURIComponent(autocompleteInput.value));
     const data = await response.json();
     console.log('Autocomplete suggestions:', data);
-  }, 500));
+
+    autocompleteList.innerHTML = '';
+
+    const suggestions = data.result.addressMatches;
+    // TODO: If suggestions is empty, let the user know no matches were found.
+    if (!suggestions || suggestions.length === 0) {
+      const noResultsItem = document.createElement('li');
+      noResultsItem.textContent = 'No matches found.';
+      autocompleteList.appendChild(noResultsItem);
+    }
+
+    for (const match of suggestions) {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `<button>${match.matchedAddress}</button>`;
+      listItem.querySelector('button').addEventListener('click', () => {
+        autocompleteInput.value = match.matchedAddress;
+        autocompleteList.innerHTML = '';
+
+        const lat = match.coordinates.y;
+        const lng = match.coordinates.x;
+        const evt = new CustomEvent('userLocationAcquired', {
+          detail: { lat, lng },
+        });
+        events.dispatchEvent(evt);
+      });
+      autocompleteList.appendChild(listItem);
+    }
+  };
+
+  const debouncedInputHandler = _.debounce(inputHandler, 500);
+  
+  autocompleteInput.addEventListener('input', debouncedInputHandler);
 }
 
 export { initGeolocation, initAutocomplete };
